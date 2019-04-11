6dd0f73ab664d760e25bffb8777c44fd1ad45b85
amitarora1991 <amitarora1991@gmail.com>
2019-04-11 22:45:32 +0530

    ghhh
    fghhh

---
+++ CP.CustomerPortal/Controllers/AppAuthorization/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using CP.CoreWeb.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CP.CustomerPortal.Controllers.AppAuthorization
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(UserInfo model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            //var result = await
+
+            return View();
+        }
+    }
+}
---
+++ CP.CustomerPortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CP.CustomerPortal.MvcApplication" Language="C#" %>
