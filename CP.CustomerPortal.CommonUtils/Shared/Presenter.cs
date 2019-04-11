90a42f5e4fb7973a6363a79bebec40a61441f868
amitarora1991 <amitarora1991@gmail.com>
2019-04-11 22:24:47 +0530

    fgg
    dghhn

---
+++ CP.CustomerPortal.CommonUtils/Shared/Presenter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.CustomerPortal.CommonUtils.Shared
+{
+    public abstract class Presenter<TView> : IDisposable
+    {
+        protected Presenter()
+        { }
+
+        public TView View { get; set; }
+
+        public virtual void OnViewInitialized()
+        { }
+
+        public virtual void OnViewLoaded()
+        { }
+
+        public virtual void Dispose()
+        { }
+    }
+}
