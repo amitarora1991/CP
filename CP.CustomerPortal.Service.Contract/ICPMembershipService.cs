98145097631c42938eedee2c4e28f44d4fbd8c51
amitarora1991 <amitarora1991@gmail.com>
2019-04-11 22:28:13 +0530

    gh
    fghjj

---
+++ CP.CustomerPortal.Service.Contract/ICPMembershipService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Security;
+namespace CP.CustomerPortal.Service.Contract
+{
+    [ServiceContract]
+    public interface ICPMembershipService
+    {
+        [OperationContract]
+        MembershipUser CreateUser(string username, string password);
+
+        [OperationContract]
+        MembershipUser CreateUser(string username, string password, string email);
+
+    }
+}
---
+++ CP.CustomerPortal.Service/CPMembershipService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+using CP.CustomerPortal.Service.Contract;
+
+namespace CP.CustomerPortal.Service
+{
+    public class CPMembershipService : ICPMembershipService
+    {
+        public MembershipUser CreateUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MembershipUser CreateUser(string username, string password, string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
