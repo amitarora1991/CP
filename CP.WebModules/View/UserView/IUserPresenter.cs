30aab8fac97103b2e3e2c28c5c739cdc3918ca88
amitarora1991 <amitarora1991@gmail.com>
2019-04-11 22:29:05 +0530

    gh
    cdghj

---
+++ CP.WebModules/View/UserView/IUserPresenter.cs
@@ -0,0 +1,20 @@
+﻿using CP.CoreWeb.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.WebModules.View.UserView
+{
+    public interface IUserPresenter
+    {
+        string SucccessMessage { get; set; }
+
+        string ErrorMessage { get; set; }
+
+        UserInfo User { get; set; }
+
+        List<UserInfo> Users { get; set; }
+    }
+}
---
+++ CP.WebModules/View/UserView/UserPresenter.cs
@@ -0,0 +1,33 @@
+﻿using CP.CoreWeb.DataContracts;
+using CP.CustomerPortal.CommonUtils.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.WebModules.View.UserView
+{
+    public class UserPresenter : Presenter<IUserPresenter>
+    {
+        public UserPresenter()
+        {
+
+        }
+
+        public void GetUser()
+        {
+
+        }
+
+        public void ValidateUser()
+        {
+
+        }
+
+        public void GetUsers()
+        {
+
+        }
+    }
+}
